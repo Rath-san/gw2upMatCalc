a481b871498e20772f182b46a9a47e17936c6d4d
Rath-san <rim.kozlowski@gmail.com>
2020-10-17 10:47:06 +0200

    Fixing select value

--- src/components/Select.js
+++ src/components/Select.js
@@ -3,12 +3,12 @@ import React from 'react';
 const Select = (props) => {
     const { onChange, value, options } = props;
     return (
-        <select onChange={onChange}>
+        <select onChange={onChange} value={value}>
             {Boolean(options.length) &&
                 options.map((item, i) => {
                     const itemValue = item.value ?? item
                     const itemName = item.name ?? item
-                    return <option selected={value === itemValue} key={i} value={itemValue}>{itemName}</option>;
+                    return <option key={i} value={itemValue}>{itemName}</option>;
                 })}
         </select>
     );
