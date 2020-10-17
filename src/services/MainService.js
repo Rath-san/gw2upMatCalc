9e64f5fccf93396ecf4d886fa98c81dcb1068dfa
Rath-san <rim.kozlowski@gmail.com>
2020-10-17 10:39:00 +0200

    Adding cache for get requests

--- src/services/MainService.js
+++ src/services/MainService.js
@@ -1,7 +1,10 @@
-import axios from 'axios';
+import { setup } from 'axios-cache-adapter';
 
-const MainService = axios.create({
-    baseURL: 'https://www.gw2spidy.com/api/v0.9/json/'
-})
+const MainService = setup({
+    baseURL: 'https://www.gw2spidy.com/api/v0.9/json/',
+    cache: {
+        maxAge: 5 * 60 * 1000
+    }
+});
 
 export default MainService;
